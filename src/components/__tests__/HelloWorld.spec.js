c04e91cda3d73fb9d9bd24f56a3dcfaa6eee4902
elaneg <elane.grd@gmail.com>
2024-08-06 18:05:25 +0200

    feat/accueilEtMenu : mise en place du menu, des routes, correction de la config

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import Cv from "@/components/Cv.vue";
 
-describe('HelloWorld', () => {
+describe('Cv', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(Cv, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
