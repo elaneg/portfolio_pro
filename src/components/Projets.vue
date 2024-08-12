323e26bb2b9ab470cb12fbb409e3c1e4cfdc4c68
elaneg <elane.grd@gmail.com>
2024-08-12 18:11:50 +0200

    feat/projets : ajustement couleur des project-card

--- src/components/Projets.vue
+++ src/components/Projets.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="projets" aria-labelledby="projects-title">
         <section>
-            <h2 id="projects-title">Mes projets<br /><br /></h2>
+            <h2 id="projects-title">Mes projets<br/><br/></h2>
             <div class="projects-grid" aria-label="Liste des projets">
                 <div
-                    v-for="project in projects"
-                    :key="project.id"
-                    class="project-card"
-                    :aria-labelledby="'project-title-' + project.id"
-                    :aria-describedby="'project-desc-' + project.id"
-                    @click="redirectToProject(project.url)"
-                    role="link"
-                    tabindex="0">
-                    <img :src="project.image" :alt="project.name" class="project-image" />
+                        v-for="project in projects"
+                        :key="project.id"
+                        class="project-card"
+                        :aria-labelledby="'project-title-' + project.id"
+                        :aria-describedby="'project-desc-' + project.id"
+                        @click="redirectToProject(project.url)"
+                        role="link"
+                        tabindex="0">
+                    <img :src="project.image" :alt="project.name" class="project-image"/>
                     <h3 :id="'project-title-' + project.id">{{ project.name }}</h3>
                     <p :id="'project-desc-' + project.id">{{ project.description }}</p>
                 </div>
@@ -27,9 +27,27 @@ export default {
     data() {
         return {
             projects: [
-                { id: 1, name: 'Lunetterie de Villeroy', description: 'Description du projet 1', url: 'https://mmi21b07.mmi-troyes.fr/ws501/', image: '../public/lunetterie.jpg' },
-                { id: 2, name: 'Projet 2', description: 'Description du projet 2', url: '/projet2', image: 'path/to/image2.jpg' },
-                { id: 3, name: 'Projet 3', description: 'Description du projet 3', url: '/projet3', image: 'path/to/image3.jpg' }
+                {
+                    id: 1,
+                    name: 'Lunetterie de Villeroy',
+                    description: 'Description du projet 1',
+                    url: 'https://mmi21b07.mmi-troyes.fr/ws501/',
+                    image: '../public/lunetterie.jpg'
+                },
+                {
+                    id: 2,
+                    name: 'Projet 2',
+                    description: 'Description du projet 2',
+                    url: '/projet2',
+                    image: 'path/to/image2.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Projet 3',
+                    description: 'Description du projet 3',
+                    url: '/projet3',
+                    image: 'path/to/image3.jpg'
+                }
             ]
         };
     },
@@ -53,13 +71,12 @@ export default {
 
 .projects-grid {
     display: grid;
-    /*grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));*/
     grid-template-columns: auto auto auto auto;
     gap: 70px;
 }
 
 .project-card {
-    background-color: #f5f5f5;
+    background-color: rgba(137, 167, 220, 0.59);
     width: 60vh;
     border-radius: 20px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
